8d25040fca5804895d1cf603bd385b7715fadfbc
sebasmazo <sebastianmazovelez@gmail.com>
2021-04-09 00:03:03 -0500

    Fin ejercicio 5

--- Unidad 2/Ejercicio5.c
+++ Unidad 2/Ejercicio5.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 int main(int argc, char *argv[])
 {
-    int enteros[argc-1], min;
+    int enteros[argc-1], min, max;
     float promedio = 0;
     for (int i = 1; i < argc; i++)
     {
@@ -10,9 +10,17 @@ int main(int argc, char *argv[])
         promedio = enteros[i-1] + promedio;
     }
     printf("%f \n", promedio/(argc-1) ); //Promedio
-    for (int i = 0; i < enteros.; i++)
+    min = enteros[0];
+    max = enteros[0];
+    for (int i = 1; i < argc-1; i++)
     {
-        min = enteros[i]
+        if(min > enteros[i]){
+            min = enteros[i];
+        }else if (max< enteros[i])
+        {
+            max = enteros[i];
+        }
     }
-    
+    printf("Minimo: %d", min);
+    printf("Maximo: %d \n", max);
 }
